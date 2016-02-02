ab2d674c2f14dceb45de3235af9a3002ba172f70
Lorenzo Ruiz <lars.bs@hotmail.com>
2016-02-02 14:34:04 +0100

    TypeParser module finished

--- test/index.js
+++ test/index.js
@@ -11,11 +11,11 @@ const TypeParser = require('../lib/utils/type-parser');
 
 describe('UNIT TESTS', function () {
   describe('GraysQL', function () {
-    require('./unit/graysql')(GraysQL);
     require('./unit/graysql/type')(Type);
     require('./unit/graysql/interface')(Interface);
     require('./unit/graysql/query')(Query);
     require('./unit/graysql/mutation')(Mutation);
+    require('./unit/graysql')(GraysQL);
     describe('@Utils', function () {
       require('./unit/utils')(Utils);
       require('./unit/utils/type-parser')(TypeParser);
--- test/unit/utils/type-parser.js
+++ test/unit/utils/type-parser.js
@@ -7,16 +7,33 @@ const graphql = require('graphql');
 module.exports = function (TypeParser) {
 
   describe('@TypeParser', function () {
+    let SimpleType;
+    before(function () {
+      SimpleType = new graphql.GraphQLObjectType({
+        name: 'Simple',
+        fields: () => ({
+          id: { type: graphql.GraphQLInt }
+        })
+      });
+    });
     describe('#parseType(type, dependencies)', function () {
-      it('should parse the types', function () {
+      it('should parse simple types: `Int`, `String`, dependencies, etc', function () {
         expect(TypeParser.parseType('Int')).to.deep.equal(graphql.GraphQLInt);
-        expect(TypeParser.parseType('Int!')).to.deep.equal(new graphql.GraphQLNonNull(graphql.GraphQLInt));
-        expect(TypeParser.parseType('[Int]')).to.deep.equal(new graphql.GraphQLList(graphql.GraphQLInt));
-        expect(TypeParser.parseType('[Int]!')).to.deep.equal(new graphql.GraphQLNonNull(new graphql.GraphQLList(graphql.GraphQLInt)));
         expect(TypeParser.parseType('String')).to.deep.equal(graphql.GraphQLString);
-        expect(TypeParser.parseType('String!')).to.deep.equal(new graphql.GraphQLNonNull(graphql.GraphQLString));
+        expect(TypeParser.parseType('Simple', { 'Simple': SimpleType })).to.deep.equal(SimpleType);
+      });
+      it('should parse array versions of the simple types', function () {
+        expect(TypeParser.parseType('[Int]')).to.deep.equal(new graphql.GraphQLList(graphql.GraphQLInt));
         expect(TypeParser.parseType('[String]')).to.deep.equal(new graphql.GraphQLList(graphql.GraphQLString));
+        expect(TypeParser.parseType('[Simple]', { 'Simple': SimpleType })).to.deep.equal(new graphql.GraphQLList(SimpleType));
+      });
+      it('should parse non nullable versions of all types', function () {
+        expect(TypeParser.parseType('Int!')).to.deep.equal(new graphql.GraphQLNonNull(graphql.GraphQLInt));
+        expect(TypeParser.parseType('[Int]!')).to.deep.equal(new graphql.GraphQLNonNull(new graphql.GraphQLList(graphql.GraphQLInt)));
+        expect(TypeParser.parseType('String!')).to.deep.equal(new graphql.GraphQLNonNull(graphql.GraphQLString));
         expect(TypeParser.parseType('[String]!')).to.deep.equal(new graphql.GraphQLNonNull(new graphql.GraphQLList(graphql.GraphQLString)));
+        expect(TypeParser.parseType('Simple!', { 'Simple': SimpleType })).to.deep.equal(new graphql.GraphQLNonNull(SimpleType));
+        expect(TypeParser.parseType('[Simple]!', { 'Simple': SimpleType })).to.deep.equal(new graphql.GraphQLNonNull(new graphql.GraphQLList(SimpleType)));
       });
     });
   });
